8577285368c2cb08e0a0865ceedff62ac138c602
Tim Shipman <bricklayertim@yahoo.com>
2017-10-31 23:17:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = "I don't do much."
 
 var kittens = ["Milo", "Otis", "Garfield"]
 
-function destructivelyAppendKitten(name) {
+function destructivelyAppendKitten(kittens) {
   array.push("Ralph")
   return array
 }
